59d306acd35994d3829b3ddc52a7601b1060b269
Justin Warren <justin@eigenmagic.com>
2024-09-29 10:13:00 +1000

    Test cases Flake8 lint fixes and Black formatting updates.

--- tests/test_configfile.py
+++ tests/test_configfile.py
@@ -1,15 +1,18 @@
 """Test the config file is loading parameters correctly
 """
+
 from util import shim_argparse
-from fediblockhole import setup_argparse, augment_args
+
+from fediblockhole import augment_args, setup_argparse
+
 
 def test_parse_tomldata():
     tomldata = """
 # Test TOML config for FediBlockHole
 
-blocklist_instance_sources = []  
+blocklist_instance_sources = []
 
-blocklist_url_sources = []  
+blocklist_url_sources = []
 
 save_intermediate = true
 
@@ -21,42 +24,49 @@ import_fields = ['public_comment']
 
     assert args.blocklist_instance_sources == []
     assert args.blocklist_url_sources == []
-    assert args.save_intermediate == True
-    assert args.import_fields == ['public_comment']
+    assert args.save_intermediate is True
+    assert args.import_fields == ["public_comment"]
+
 
 def test_set_mergeplan_max():
     tomldata = """mergeplan = 'max'
     """
     args = shim_argparse([], tomldata)
 
-    assert args.mergeplan == 'max'
+    assert args.mergeplan == "max"
+
 
 def test_set_mergeplan_min():
     tomldata = """mergeplan = 'min'
     """
     args = shim_argparse([], tomldata)
 
-    assert args.mergeplan == 'min'
+    assert args.mergeplan == "min"
+
 
 def test_set_allowlists():
     tomldata = """# Comment on config
-allowlist_url_sources = [ { url='file:///path/to/allowlist', format='csv'} ] 
+allowlist_url_sources = [ { url='file:///path/to/allowlist', format='csv'} ]
 """
     args = shim_argparse([], tomldata)
 
-    assert args.mergeplan == 'max'
-    assert args.allowlist_url_sources == [{
-        'url': 'file:///path/to/allowlist',
-        'format': 'csv',
-        }]
+    assert args.mergeplan == "max"
+    assert args.allowlist_url_sources == [
+        {
+            "url": "file:///path/to/allowlist",
+            "format": "csv",
+        }
+    ]
+
 
 def test_set_merge_thresold_default():
     tomldata = """
 """
     args = shim_argparse([], tomldata)
 
-    assert args.mergeplan == 'max'
-    assert args.merge_threshold_type == 'count'
+    assert args.mergeplan == "max"
+    assert args.merge_threshold_type == "count"
+
 
 def test_set_merge_thresold_count():
     tomldata = """# Add a merge threshold
@@ -65,9 +75,10 @@ merge_threshold = 2
 """
     args = shim_argparse([], tomldata)
 
-    assert args.mergeplan == 'max'
-    assert args.merge_threshold_type == 'count'
+    assert args.mergeplan == "max"
+    assert args.merge_threshold_type == "count"
     assert args.merge_threshold == 2
+
 
 def test_set_merge_thresold_pct():
     tomldata = """# Add a merge threshold
@@ -76,6 +87,6 @@ merge_threshold = 35
 """
     args = shim_argparse([], tomldata)
 
-    assert args.mergeplan == 'max'
-    assert args.merge_threshold_type == 'pct'
+    assert args.mergeplan == "max"
+    assert args.merge_threshold_type == "pct"
     assert args.merge_threshold == 35
